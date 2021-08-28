fa34af1dd469106b745ba6c597d1a127978dd8e1
Deathstar8 <49576593+Deathstar8@users.noreply.github.com>
2021-08-28 11:28:01 -0500

    Update SmoothScrollingEverywhereMM.java

--- src/main/java/me/shedaniel/smoothscrollingeverywhere/SmoothScrollingEverywhereMM.java
+++ src/main/java/me/shedaniel/smoothscrollingeverywhere/SmoothScrollingEverywhereMM.java
@@ -1,18 +1,17 @@
 package me.shedaniel.smoothscrollingeverywhere;
 
-import io.github.prospector.modmenu.api.ConfigScreenFactory;
-import io.github.prospector.modmenu.api.ModMenuApi;
-import me.shedaniel.clothconfig2.ClothConfigInitializer;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
 import net.minecraft.text.LiteralText;
 
 public class SmoothScrollingEverywhereMM implements ModMenuApi {
-    @Override
+ 
     public String getModId() {
         return "smoothscrollingeverywhere";
     }
     
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> ClothConfigInitializer.getConfigBuilder().setTitle(new LiteralText("Smooth Scrolling Settings")).build();
+        return parent -> SmoothScrollingEverywhere.getConfigBuilder().setTitle(new LiteralText("Smooth Scrolling Settings")).build();
     }
 }
